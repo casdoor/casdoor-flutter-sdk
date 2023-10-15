074f80c82441d63449074c9945d41223bac62bf3
Mario Fischer <klicker-mail@gmx.net>
2023-10-15 12:50:08 +0200

    fix: add exceptions to library import file

--- lib/casdoor_flutter_sdk.dart
+++ lib/casdoor_flutter_sdk.dart
@@ -1,3 +1,4 @@
 export 'src/casdoor.dart';
 export 'src/casdoor_flutter_sdk_config.dart';
+export 'src/casdoor_flutter_sdk_exceptions.dart';
 export 'src/casdoor_flutter_sdk_platform_interface.dart';
