aa1e1cbd9f3cd540a3d3e2dba1dd46d83e4a1a9f
Mario Fischer <klicker-mail@gmx.net>
2023-10-16 07:40:15 +0200

    fix: add plugins to library import file

--- lib/casdoor_flutter_sdk.dart
+++ lib/casdoor_flutter_sdk.dart
@@ -1,4 +1,7 @@
 export 'src/casdoor.dart';
 export 'src/casdoor_flutter_sdk_config.dart';
+export 'src/casdoor_flutter_sdk_desktop.dart';
 export 'src/casdoor_flutter_sdk_exceptions.dart';
+export 'src/casdoor_flutter_sdk_mobile.dart';
 export 'src/casdoor_flutter_sdk_platform_interface.dart';
+export 'src/casdoor_flutter_sdk_web.dart';
