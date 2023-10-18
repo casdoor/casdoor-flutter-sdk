f327fa7d9dcb825fb280f2dac82f8897f0741bb3
Mario Fischer <klicker-mail@gmx.net>
2023-10-18 12:04:10 +0200

    fix: missing plugin references in library import file

--- lib/casdoor_flutter_sdk.dart
+++ lib/casdoor_flutter_sdk.dart
@@ -1,4 +1,6 @@
 export 'src/casdoor.dart';
 export 'src/casdoor_flutter_sdk_config.dart';
+export 'src/casdoor_flutter_sdk_desktop.dart';
 export 'src/casdoor_flutter_sdk_exceptions.dart';
+export 'src/casdoor_flutter_sdk_mobile.dart';
 export 'src/casdoor_flutter_sdk_platform_interface.dart';
