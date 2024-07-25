fbfc5c7822cef95240124a5f16e78be8b517c727
Max <maxmeng93@gmail.com>
2024-07-25 18:02:36 +0800

    feat: update dependency (#42)

--- lib/src/casdoor_flutter_sdk_mobile.dart
+++ lib/src/casdoor_flutter_sdk_mobile.dart
@@ -76,15 +76,14 @@ class _FullScreenAuthPageState extends State<FullScreenAuthPage> {
     return Stack(
       children: [
         InAppWebView(
-          initialUrlRequest: URLRequest(url: Uri.parse(widget.params.url)),
-          initialOptions: InAppWebViewGroupOptions(
-            crossPlatform: InAppWebViewOptions(
-              userAgent:
-                  "Mozilla/5.0 (Android 14; Mobile; rv:123.0) Gecko/123.0 Firefox/123.0",
-              clearCache: widget.params.clearCache!,
-              useShouldOverrideUrlLoading: true,
-              useOnLoadResource: true,
-            ),
+          initialUrlRequest:
+              URLRequest(url: WebUri.uri(Uri.parse(widget.params.url))),
+          initialSettings: InAppWebViewSettings(
+            userAgent:
+                "Mozilla/5.0 (Android 14; Mobile; rv:123.0) Gecko/123.0 Firefox/123.0",
+            clearCache: widget.params.clearCache!,
+            useShouldOverrideUrlLoading: true,
+            useOnLoadResource: true,
           ),
           shouldOverrideUrlLoading: (controller, navigationAction) async {
             final uri = navigationAction.request.url!;
@@ -207,25 +206,19 @@ class CasdoorFlutterSdkMobile extends CasdoorFlutterSdkPlatform {
     });
 
     await browser.openUrlRequest(
-      urlRequest: URLRequest(url: Uri.parse(params.url)),
-      options: InAppBrowserClassOptions(
-        inAppWebViewGroupOptions: InAppWebViewGroupOptions(
-          crossPlatform: InAppWebViewOptions(
-            userAgent:
-                "Mozilla/5.0 (Android 14; Mobile; rv:123.0) Gecko/123.0 Firefox/123.0",
-            clearCache: params.clearCache!,
-            useOnLoadResource: true,
-            useShouldOverrideUrlLoading: true,
-          ),
+      urlRequest: URLRequest(url: WebUri.uri(Uri.parse(params.url))),
+      settings: InAppBrowserClassSettings(
+        webViewSettings: InAppWebViewSettings(
+          userAgent:
+              "Mozilla/5.0 (Android 14; Mobile; rv:123.0) Gecko/123.0 Firefox/123.0",
+          clearCache: params.clearCache!,
+          useOnLoadResource: true,
+          useShouldOverrideUrlLoading: true,
         ),
-        crossPlatform: InAppBrowserOptions(
+        browserSettings: InAppBrowserSettings(
           hideUrlBar: true,
           toolbarTopBackgroundColor: Colors.grey.shade300,
-        ),
-        android: AndroidInAppBrowserOptions(
           toolbarTopFixedTitle: 'Login',
-        ),
-        ios: IOSInAppBrowserOptions(
           hideToolbarBottom: true,
         ),
       ),
